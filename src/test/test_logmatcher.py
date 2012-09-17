8826a45b15a80f1695211a5b32f9ac5ef9f3d7b0
Keita Kita <maoutwo@gmail.com>
2012-09-17 20:16:16 +0900

    Add test for matching.

---
+++ src/test/test_logmatcher.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import dummy_threading
+import threading
+import unittest
+
+import logmatcher
+
+class MockLogcatThread(dummy_threading.Thread):
+    u'''
+    LogcatThread that does not execute adb.
+    '''
+
+    def __init__(self):
+        dummy_threading.Thread.__init__(self)
+        self.isTerminated = False
+
+    def terminate(self):
+        self.isTerminated = True
+
+class MockLogMatcher(logmatcher.LogMatcher):
+    u'''
+    LogMatcher that creates MockLogcatThread.
+    '''
+
+    def createLogcatThread(self):
+        return MockLogcatThread()
+
+    def createMatchedEvent(self):
+        return dummy_threading.Event()
+
+class TestMatchingLog(unittest.TestCase):
+    u'''
+    Test matching log.
+    '''
+
+    def setUp(self):
+        self.__matcher = MockLogMatcher()
+        self.__matcher.start()
+
+    def testFirstMatched(self):
+        u'''
+        LogMatcher does not match just after LogMatcher is created.
+        '''
+        self.assert_(not self.__matcher.isMatched())
+
+    def testNotMatchedWhenNoLog(self):
+        u'''
+        LogMatcher does not match when it did not receive any log.
+        '''
+        self.assert_(not self.__matcher.wait(u'match', 0.1))
+
+    def testNotMatchedWhenNotMatchedLog(self):
+        u'''
+        LogMatcher does not match when it did not receive matched log.
+        '''
+        self.__matcher.onLogReceived(u'matched')
+
+        self.assert_(not self.__matcher.wait(u'not matched', 0.1))
+
+    def testMatchedBeforeWaiting(self):
+        u'''
+        LogMatcher matches before waiting to receive log.
+        '''
+        self.__matcher.onLogReceived(u'matc')
+        self.__matcher.onLogReceived(u'hing')
+
+        self.assert_(self.__matcher.wait(u'match', 0.1))
+
+    def testMatchingAfterWaiting(self):
+        u'''
+        LogMatcher matches after waiting to receive log.
+        '''
+        def sendLog(logMatcher):
+            logMatcher.onLogReceived(u"matched")
+
+        threading.Timer(1, sendLog, [self.__matcher]).start()
+        self.assert_(self.__matcher.wait(u'match', 2))
+
+if __name__ == '__main__':
+    unittest.main()
